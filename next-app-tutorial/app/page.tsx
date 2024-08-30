633943168f2c30052e38d1cd98e93b1a6c0ed1ed
Katrina W. <katrina.elise.wright@gmail.com>
2024-08-30 12:11:59 -0500

    grab dummy users and add to page

--- next-app-tutorial/app/page.tsx
+++ next-app-tutorial/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import ProductCard from './components/ProductCard'
 
 export default function Home() {
   return (
     <main>
       <h1>New Project Huzzah</h1>
       <Link href="/users">Users</Link> {/* #Client side navigation, use next.js Link va a */}
+      <ProductCard></ProductCard>
     </main>
   )
 }
