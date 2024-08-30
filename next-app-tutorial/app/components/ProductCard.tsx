2d850af1416ca81d7b58ca8000e4a4fb48a34ab5
Katrina W. <katrina.elise.wright@gmail.com>
2024-08-30 12:40:02 -0500

    tailwind implementation instead

--- next-app-tutorial/app/components/ProductCard.tsx
+++ next-app-tutorial/app/components/ProductCard.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import AddToCart from './AddToCart';
-import styles from './ProductCard.module.css';
+// import styles from './ProductCard.module.css';
 
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
+    <div className='p-5 my-5 bg-sky-400 text-white texl-xl hover:bg-sky-500'>
         <div>ProductCard</div>
         <AddToCart/>
     </div>
